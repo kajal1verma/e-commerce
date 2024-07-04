3e836d3737394c158db75eb91778ff18423b96d3
kajal1verma <kajal5219v@gmail.com>
2024-07-04 09:53:02 +0530

    ecommerce

---
+++ script.js
@@ -0,0 +1,19 @@
+// init Isotope
+let $grid = $('.collection-list').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+  // console.log("kkk")
+  let $grid1 = $('.filter').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid1.isotope({ filter: filterValue });
+  });
+  // console.log("kkk")
